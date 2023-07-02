4dcdc319d4bf33efc5a414f1312091f2c0336b96
Philip Scheetz <philip@scheetznet.com>
2023-07-01 21:40:37 -0600

    Fixed dumb user error

--- azure/cloudlee-az104-course/html/script-extension.ps1
+++ azure/cloudlee-az104-course/html/script-extension.ps1
@@ -16,5 +16,5 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
 # Download web files
-Invoke-WebRequest Invoke-WebRequest https://raw.githubusercontent.com/pscheetz/code-projects/master/azure/cloudlee-az104-course/html/index.html -OutFile C:\Users\Philip\Desktop\index.html -OutFile C:\inetpub\wwwroot\index.htm
-Invoke-WebRequest Invoke-WebRequest https://raw.githubusercontent.com/pscheetz/code-projects/master/azure/cloudlee-az104-course/html/style.css -OutFile C:\Users\Philip\Desktop\style.css -OutFile C:\inetpub\wwwroot\style.css
+Invoke-WebRequest Invoke-WebRequest https://raw.githubusercontent.com/pscheetz/code-projects/master/azure/cloudlee-az104-course/html/index.html -OutFile C:\inetpub\wwwroot\index.html
+Invoke-WebRequest Invoke-WebRequest https://raw.githubusercontent.com/pscheetz/code-projects/master/azure/cloudlee-az104-course/html/style.css -OutFile C:\inetpub\wwwroot\style.css
