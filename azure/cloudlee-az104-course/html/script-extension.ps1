576c8700a119d863587826ed16507c0b1cd1a30b
Philip Scheetz <philip@scheetznet.com>
2023-07-01 21:25:37 -0600

    Added Instructions for Custom Script Extension

--- azure/cloudlee-az104-course/html/script-extension.ps1
+++ azure/cloudlee-az104-course/html/script-extension.ps1
@@ -1,5 +1,17 @@
 # Custom Script Extension to Install IIS Web Server and provision it with custom HTML/CSS
 
+# Instructions
+# 1. Browse to the VM; ensure it is powered on
+# 2. Go to 'Extensions + applications'
+# 3. Click 'Add'; search for "Custom Script Extension" and click it
+# 4. Click Next. 
+# 5. Browse to the script in your Storage Account and click 'select'
+#     (you can specify arguments if you have any in your script)
+# 6. Click 'Next: Review + Create' then 'Create'
+
+
+
+
 # Install IIS 
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
